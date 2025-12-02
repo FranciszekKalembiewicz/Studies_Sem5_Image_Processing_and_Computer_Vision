c06ed6d8e9f590ce0a237c2602b1217b12303e50
Franciszek Kalembkiewicz <f.kalembkiewicz@wp.pl>
2025-12-02 19:34:07 +0100

    recap

--- Lab6/Lab6.py
+++ Lab6/Lab6.py
@@ -4,10 +4,7 @@ import matplotlib.pyplot as plt
 from scipy import ndimage as ndi
 
 def otsu_threshold(image):
-    if len(image.shape) == 3:
-        image = np.array(Image.fromarray(image).convert('L'))
-    else:
-        image = np.array(image)
+    image = np.array(image)
 
     hist, _ = np.histogram(image.flatten(), bins=256, range=[0, 256])
     hist = hist.astype(float)
@@ -25,50 +22,49 @@ def otsu_threshold(image):
             continue
         mu0 = np.sum(np.arange(0, k + 1) * p[:k + 1]) / w0
         mu1 = np.sum(np.arange(k + 1, 256) * p[k + 1:]) / w1
-        sigma_b = w0 * w1 * (mu1 - mu0) ** 2
-        if sigma_b > max_sigma:
-            max_sigma = sigma_b
+        sigma_new = w0 * w1 * (mu1 - mu0) ** 2
+        if sigma_new > max_sigma:
+            max_sigma = sigma_new
             threshold = k
     return threshold
 
-
-def apply_threshold(image, thresh):
-    if len(image.shape) == 3:
-        image = np.array(Image.fromarray(image).convert('L'))
-    return (image > thresh).astype(np.uint8) * 255
-
-def watershed_segmentation(binary_image):
-    mask = binary_image > 0
-    distance = ndi.distance_transform_edt(mask)
-    local_max = ndi.maximum_filter(distance, size=20) == distance
-    markers, _ = ndi.label(local_max)
-    labels = ndi.watershed_ift(binary_image.astype(np.uint8), markers)
-    return labels
-
 def process_image(path):
-    image = np.array(Image.open(path))
+    img = np.array(Image.open(path).convert('L'))
 
-    thresh = otsu_threshold(image)
-    binary_image = apply_threshold(image, thresh)
+    thresh = otsu_threshold(img)
+    binary = img > thresh
 
-    labels = watershed_segmentation(binary_image)
+    foreground = ndi.binary_erosion(binary, iterations=2)
+    background = ndi.binary_dilation(~binary, iterations=2)
 
-    fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-    axs[0].imshow(image, cmap='gray')
-    axs[0].set_title('Oryginalny obraz')
-    axs[0].axis('off')
+    markers = np.zeros(img.shape, dtype=np.int32)
+    markers[background] = 1
+    markers[foreground] = 2
 
-    axs[1].imshow(binary_image, cmap='gray')
-    axs[1].set_title(f'Binaryzacja Otsu (thresh={thresh})')
-    axs[1].axis('off')
+    labels = ndi.watershed_ift((binary*255).astype(np.uint8), markers)
 
-    axs[2].imshow(labels, cmap='nipy_spectral')
-    axs[2].set_title('Segmentacja Watershed')
-    axs[2].axis('off')
+    markers_rgb = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    markers_rgb[foreground] = [0, 0, 255]
+    markers_rgb[background] = [255, 0, 0]
+
+    plt.figure(figsize=(12, 4))
+
+    plt.subplot(1, 3, 1)
+    plt.title("Oryginał")
+    plt.imshow(img, cmap='gray')
+    plt.axis('off')
+
+    plt.subplot(1, 3, 2)
+    plt.title(f"Markery kolorowe (Otsu k={thresh})")
+    plt.imshow(markers_rgb)
+    plt.axis('off')
+
+    plt.subplot(1, 3, 3)
+    plt.title("Watershed")
+    plt.imshow(labels, cmap='nipy_spectral')
+    plt.axis('off')
 
     plt.show()
 
-image_paths = ['coins.jpg', 'cells.jpg']
-
-for path in image_paths:
+for path in ['coins.jpg', 'coins_together.jpg']:
     process_image(path)
